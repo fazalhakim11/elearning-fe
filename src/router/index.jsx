9133087724ad1f451c682e74d63a3d0f63960b53
Faza Alhakim <fazafal04@gmail.com>
2024-08-12 15:30:13 +0700

    add errorPage

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,51 +1,57 @@
-import {
-    createBrowserRouter,
-    RouterProvider,
-} from "react-router-dom"
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import Login from "../pages/login"
-import Signup from "../pages/signup"
-import Classes from "../pages/home"
-import Subjects from "../pages/subjects"
-import Chapters from "../pages/chapters"
-import SubChapters from "../pages/subChapters"
-import Materials from "../pages/materials"
-
+import Login from "../pages/login";
+import Signup from "../pages/signup";
+import Classes from "../pages/home";
+import Subjects from "../pages/subjects";
+import Chapters from "../pages/chapters";
+import SubChapters from "../pages/subChapters";
+import Materials from "../pages/materials";
+import Main from "../pages/main";
+import ErrorPage from "../pages/error";
 
 const index = () => {
-    const router = createBrowserRouter([
-        {
-          path: "/",
-          element: <Login/>,
-        },
-        {
-          path: "/signup",
-          element: <Signup/>
-        },
-        {
-        path: "/classes",
-        element: <Classes/>
-        },
-        {
-        path: "/subjects",
-        element: <Subjects/>
-        },
-        {
-        path: "/chapters",
-        element: <Chapters/>
-        },
-        {
-        path: "/sub_chapters",
-        element: <SubChapters/>
-        },
-        {
-        path: "/materials",
-        element: <Materials/>
-        },
-      ])
-    return (
-      <RouterProvider router={router} />
-    )
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main />,
+      errorElement: <ErrorPage notFound/>
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/signup",
+      element: <Signup />,
+    },
+    {
+      path: "/classes",
+      element: <Classes />,
+      errorElement: <ErrorPage />
+    },
+    {
+      path: "/subjects",
+      element: <Subjects />,
+      errorElement: <ErrorPage />
+    },
+    {
+      path: "/chapters",
+      element: <Chapters />,
+      errorElement: <ErrorPage />
+    },
+    {
+      path: "/sub_chapters",
+      element: <SubChapters />,
+      errorElement: <ErrorPage />
+    },
+    {
+      path: "/materials",
+      element: <Materials />,
+      errorElement: <ErrorPage />
+    },
+  ]);
+  return <RouterProvider router={router} />;
 };
 
-export default index
+export default index;
