d3588b9ded12e995ea196455296f17b55e3a0073
Faza Alhakim <fazafal04@gmail.com>
2024-08-13 08:18:59 +0700

    fix youtube embed

--- src/components/youTubeEmbed/index.jsx
+++ src/components/youTubeEmbed/index.jsx
@@ -1,46 +1,51 @@
 import axios from "axios";
+import { useEffect } from "react";
 
-const YouTubeEmbed = (props)=>{ 
-
-  const userId = props.userId
+const YouTubeEmbed = (props) => {
+  const userId = props.userId;
 
   const onPlayerReady = () => {
-    console.log("Player is ready")
-  }
+    console.log("Player is ready");
+  };
 
   const onPlayerStateChange = async (event) => {
-    if ( event.data === YT.PlayerState.ENDED){
+    if (event.data === YT.PlayerState.ENDED) {
       try {
         await axios.post(`${import.meta.env.VITE_API_URL}/api/progress`, {
-          userId: userId,          
-          materialId: parseInt(props.id, 10)
-        })
-        console.log("Updating progress success..")
+          userId: userId,
+          materialId: parseInt(props.id, 10),
+        });
+        console.log("Updating progress success..");
       } catch (err) {
-        console.log(err)
+        console.log(err);
       }
     }
-  }
+  };
 
   const onYouTubeIframeAPIReady = () => {
     const player = new YT.Player(props.id, {
       width: "100%",
       videoId: props.videoId,
       playerVars: {
-        controls: 1
+        controls: 1,
       },
       events: {
         onReady: onPlayerReady,
-        onStateChange: onPlayerStateChange
-      }
-    })
-  }
-  
-  onYouTubeIframeAPIReady()
+        onStateChange: onPlayerStateChange,
+      },
+    });
+  };
+
+  useEffect(()=> {
+    onYouTubeIframeAPIReady();
+  },[userId, props.videoId, props.id])
+
 
   return (
-    <div id={props.id} ><i>Youtube Video</i></div>
-  )
+    <div id={props.id}>
+      <i>Loading Youtube Video...</i>
+    </div>
+  );
 };
 
 export default YouTubeEmbed;
