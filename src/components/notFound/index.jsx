34e3a2dfeef065867ac65381d9722fdc793bd9e4
Faza Alhakim <fazafal04@gmail.com>
2024-08-17 17:33:49 +0700

    Edit not found page

--- src/components/notFound/index.jsx
+++ src/components/notFound/index.jsx
@@ -1,12 +1,21 @@
+import img from "../../assets/image/Grade Image (2).png";
 
 const index = (props) => {
   return (
-    <div className=" flex justify-center h-screen">
-        <div className="mt-[25vh] h-max rounded-lg p-5">
-            <p className="bg-[#ca4242] p-3 rounded-lg text-sm font-bold text-white">{props.name} is Not Available</p>
+    <div className="h-max">
+      <div className="rounded-lg p-5 flex flex-col justify-center">
+        <img src={img} alt="Not Found" className="mdd:max-w-[350px] mx-auto" />
+        <div className="mdd:self-center">
+          <h1 className="text-center font-bold text-lg text-[#ca4242]">
+            Oops..
+          </h1>
+          <p className="text-center text-sm font-bold text-[#ca4242]">
+            {props.name} is Not Available
+          </p>
         </div>
+      </div>
     </div>
-  )
+  );
 };
 
-export default index
+export default index;
