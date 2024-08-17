d4e0e8b84710f503d30ab3180be5dc9ca01f1bec
Faza Alhakim <fazafal04@gmail.com>
2024-08-17 17:52:25 +0700

    Edit header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -8,9 +8,9 @@ const index = (props) => {
         return result
     }
   return (
-    <header className="bg-[#4f7ff0] rounded-b-xl mb-5 p-3">
-        <h1 className="text-2xl text-white mb-1 font-bold">{props.name}</h1>
-        <p className="text-white text-sm">Please select a {singularForm()} you want to study</p>
+    <header className="bg-white rounded-b-xl mb-5 p-3 drop-shadow-xl">
+        <h1 className="text-2xl text-black mb-1 font-bold">{props.name}</h1>
+        <p className="text-sm text-black">Please select a {singularForm()} you want to study</p>
     </header>
   )
 };
