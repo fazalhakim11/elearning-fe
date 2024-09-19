4fbf2b429e9e6c6675cf4cb2ceef43341e636054
Faza Alhakim <fazafal04@gmail.com>
2024-09-19 19:23:53 +0700

    Add logout

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react";
-import { jwtDecode } from "jwt-decode";
+import { useEffect } from "react";
+import axios from "axios";
 
 import { useGetData } from "../../hooks/useGetData";
 import LearningMode from "./learningMode";
@@ -7,6 +7,7 @@ import Loading from "../../components/loading";
 import useDataStores from "../../stores/dataStores";
 
 import image1 from "../../assets/image/Grade Image (1).png";
+import { useNavigate } from "react-router-dom";
 
 const Home = (props) => {
   const [isLoading, data, user, userId, getClasses, getToken] = useGetData();
@@ -21,6 +22,19 @@ const Home = (props) => {
   const handleBtn = (kelasId) => {
     setIsToggle(!isToggle);
     setKelasId(kelasId);
+  };
+
+  const navigate = useNavigate()
+  const handleLogout = async () => {
+    try {
+      if (!confirm("Are sure you want to logout?")) return
+      await axios.delete(`${import.meta.env.VITE_API_URL}/api/auth/logout`, {
+        withCredentials: true,
+      });
+      navigate("/home")
+    } catch (error) {
+      console.error(error);
+    } 
   };
 
   const renderClasses = () => {
@@ -50,15 +64,14 @@ const Home = (props) => {
               </div>
             ))}
           </div>
-        ) : ((
-            <div className=" flex justify-center h-screen">
-              <div className="mt-[25vh] h-max bg-[#4773d9] rounded-lg p-5">
-                <p className="bg-[#426bca] p-5 rounded-lg text-xl font-bold text-white">
-                  Loading...
-                </p>
-              </div>
+        ) : (
+          <div className=" flex justify-center h-screen">
+            <div className="mt-[25vh] h-max bg-[#4773d9] rounded-lg p-5">
+              <p className="bg-[#426bca] p-5 rounded-lg text-xl font-bold text-white">
+                Loading...
+              </p>
             </div>
-          )
+          </div>
         )}
       </>
     );
@@ -70,13 +83,21 @@ const Home = (props) => {
         <Loading home />
       ) : (
         <>
-          <header className="bg-white p-5 md:px-[5rem] mb-5 rounded-b-xl drop-shadow-xl">
-            <h1 className="text-2xl font-bold text-black">
-              Wellcome, {firstName}!
-            </h1>
-            <h1 className=" text-lg font-medium text-black">
-              Please select a grade
-            </h1>
+          <header className="flex justify-between bg-white p-5 md:px-[5rem] mb-5 rounded-b-xl drop-shadow-xl">
+            <div>
+              <h1 className="text-2xl font-bold text-black">
+                Wellcome, {firstName}!
+              </h1>
+              <h1 className=" text-lg font-medium text-black">
+                Please select a grade
+              </h1>
+            </div>
+            <button
+              className="font-semibold h-max self-center"
+              onClick={handleLogout}
+            >
+              Logout
+            </button>
           </header>
           {renderClasses()}
         </>
